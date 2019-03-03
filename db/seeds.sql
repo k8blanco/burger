25a7bd883484c87fff04de74e6e1059fd9a9e177
k8blanco <katiewhite303@gmail.com>
2019-03-02 19:28:04 -0700

    app skeleton

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ("Black Bean");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon Cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Buffalo Burger");
+
+SELECT * FROM burgers_db.burgers;
