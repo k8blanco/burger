a03cfd9f742a578531ff1a70700aac9353d6a005
k8blanco <katiewhite303@gmail.com>
2019-03-04 18:12:54 -0700

    linked to jawsdb for heroku db access

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
 //Require dotenv for db control
 require("dotenv").config();
 
-var connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB
+  });
+};
+
 
 // Make connection.
 connection.connect(function (err) {
